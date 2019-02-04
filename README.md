fbbf61773951d9a69337c6b86494d7eb30976060
tduangbo <tduangbo@asu.edu>
2019-02-03 23:07:49 -0800

    testing

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# ASUWildlifeRefuge.github.io
+# Setup:
+
+### Windows:
+- Clone the repo to your device
+- From the root of the project directory run `npm install`
+- Then run `npm start` to compile the project and launch the development server
+
+### Mac OSX:
+- Clone the repo to your device
+- Inside of `package.json` change the line `"start": "set HTTPS=true&&PORT=3000 react-scripts start" ` ->  `"start": "export HTTPS=true&&react-scripts start"`
+- From the root of the project directory run `./MacInstall.sh` to install and run the project
+- For all subsequent launches, run `npm start` to compile the project and launch the development server
+
+
+# Use:
+
+### Run npm start to start the development server
+Open [https://localhost:3000](https://localhost:3000) to view it in the browser.
+
+The page will reload changes made when you save your code.<br>
+You will also see any lint errors in the console.
+
+# SPR Use/Testing:
+
+Open the SPR Author and put [https://localhost:3000](https://localhost:3000) as the source url for a Blank Component. Then all authoring tools should preliminarily work. (Make sure use put https and not http, it will not work otherwise)
+
+## Demo:
+![Short SPR Demo](http://g.recordit.co/RkcyqPE0EZ.gif)
---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, { Component, Fragment } from "react";
+import "./css/App.css";
+import WelcomePage from "./components/WelcomePage/WelcomePage";
+import UIBar from "./components/UI/UIBar";
+import StudentView from "./components/StudentView";
+import AuthorView from "./components/AuthorView";
+import Grid from "@material-ui/core/Grid";
+import styled from "styled-components";
+import Drawer from "./components/UI/Drawer";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import BalancePopulation from "./components/BalancePopulation/BalancePopulation";
+import ViewControl from "./components/ViewControl";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+`;
+
+class App extends Component {
+  state = {
+    loggedIn: false
+  }
+
+  handleLogin = () => {
+    if (!this.state.loggedIn) {
+      this.setState({ loggedIn: true });
+    }
+  }
+
+  render () {
+    return (
+      <Router>
+        <Container className='appRoot'>
+          <Route exact path='/' component={WelcomePage} />
+          <Route
+            path='/sim'
+            component={() => (
+              <Fragment>
+                <UIBar />
+                <StudentView />
+                <Drawer
+                  Content={() => (
+                    <Fragment>
+                      <Route component={BalancePopulation} />
+                      <Route component={ViewControl} />
+                    </Fragment>
+                  )}
+                />
+              </Fragment>
+            )}
+          />
+        </Container>
+      </Router>
+    );
+  }
+}
+
+export default App;
